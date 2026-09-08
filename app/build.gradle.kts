e0460e4d73829fdcf3a319084c86e1bfc34d39c0
Hilmi Güner <94684527+hilmiguner@users.noreply.github.com>
2026-09-08 12:10:49 +0300

    build: use AGP built-in Kotlin in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -41,10 +40,6 @@ android {
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 dependencies {
